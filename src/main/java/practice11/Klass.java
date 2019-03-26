42e27abbaa8a69f19dc299d27b449238ae6ec5bd
zero <fll_005@163.com>
2019-03-26 22:23:20 +0800

    finish the practice 5 and 11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,54 @@
 package practice11;
 
-public class Klass {
+import java.util.Observable;
+
+public class Klass extends Observable{
+    private int number;
+    private Student leader;
+    boolean flag;
+
+    public Klass(int number){
+        this.number = number;
+    }
+    public int getNumber(){
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student){
+        if (!student.getKlass().flag){
+            System.out.print("It is not one of us.\n");
+        }else {
+            student.getKlass().leader = student;
+            this.leader = student;
+        }
+        setChanged();
+        notifyObservers();
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void appendMember(Student student){
+        student.getKlass().flag= true;
+        setChanged();
+        notifyObservers();
+    }
+
+    public boolean isIn(Student student){
+        if (student.getKlass().number ==this.number){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,58 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Teacher extends Person implements Observer {
+    private LinkedList<Klass> klass;
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age , LinkedList<Klass> klass){
+        super(id, name, age);
+        this.klass= klass;
+    }
+    public LinkedList getClasses(){
+        return klass;
+    }
+
+    public String introduce(){
+        if (klass!=null){
+            int size = klass.size();
+            String classes="";
+            for(int i = 0; i < size-1; i++){
+                classes = klass.get(i).getNumber()+", ";
+            }
+            classes = classes + klass.get(size-1).getNumber();
+            return super.introduce() + " I am a Teacher. I teach Class " + classes + ".";
+        }
+        else{
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student){
+        if(this.klass.contains(student.getKlass())){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }
+        else{
+            return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
+
+    public boolean isTeaching(Student student){
+        if(this.klass.contains(student.getKlass())){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.print("I am " + this.getName() + ". I know Jerry " + (((Klass) o).getLeader() == null ? "has joined " : "become Leader of ") + "Class 2.\n");
+    }
 }
