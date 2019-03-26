4a3bdbaa2734a9256a70376f5b355c6ce3cc4ea6
zero <fll_005@163.com>
2019-03-26 13:41:37 +0800

    finish the practice7,8,9
    Signed-off-by: zero <fll_005@163.com>

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,31 @@
 package practice08;
 
 public class Person {
+    private String name;
+    private int age,id;
+
+    public Person(int id,String name,int age){
+        this.name=name;
+        this.age=age;
+        this.id = id;
+    }
+    public String  getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public int getId(){
+        return id;
+    }
+    public String introduce(){
+        return "My name is "+ name +". I am "+ age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Person s =  (Person) obj;
+        return this.id==s.getId();
+    }
 }
+
--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,38 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    boolean flag;
+
+    public Klass(int number){
+        this.number = number;
+    }
+    public int getNumber(){
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student){
+        if (!student.getKlass().flag){
+            System.out.print("It is not one of us.\n");
+        }else {
+            student.getKlass().leader = student;
+        }
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void appendMember(Student student){
+        student.getKlass().flag= true;
+    }
 }
