100a92ada51355bff050cff7b7ebd69b1e652255
zero <fll_005@163.com>
2019-03-26 21:23:31 +0800

    commit for practice 10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,23 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+    public Student( int id,String name,int age, Klass klass) {
+        super(id, name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if (klass.getLeader() == null){
+            return super.introduce()+" I am a Student. I am at Class "+ klass.getNumber()+".";
+        }
+        else{
+            return super.introduce()+" I am a Student. I am Leader of Class "+ klass.getNumber()+".";
+        }
+
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,52 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> klass;
+    public Teacher(int id,String name,int age) {
+        super(id,name,age);
+    }
+
+    public Teacher(int id,String name,int age ,LinkedList<Klass> klass){
+        super(id,name,age);
+        this.klass= klass;
+    }
+    public LinkedList getClasses(){
+        return klass;
+    }
+
+    public String introduce(){
+        if (klass!=null){
+            int size = klass.size();
+            String classes="";
+            for(int i=0; i<size-1; i++){
+                classes = klass.get(i).getNumber()+", ";
+            }
+            classes = classes+klass.get(size-1).getNumber();
+            return super.introduce()+" I am a Teacher. I teach Class "+ classes+".";
+        }
+        else{
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student){
+        if(this.klass.contains(student.getKlass())){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }
+        else{
+            return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
+
+    public boolean isTeaching(Student student){
+        if(this.klass.contains(student.getKlass())){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
 }
